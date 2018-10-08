50805d09d578469abfb4c8f001ad67105f1b5e9a
snarksliveshere <snarksliveshere@gmail.com>
2018-10-08 20:41:53 +0300

    initial commit

---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction(Request $request)
+    {
+        $a = 42;
+        return $this->render('default/index.html.twig', compact('a'));
+    }
+
+
+}
